ae18fb936c019ffe87dd56e0a3f5c76330c5c1b9
Tony <antony.branquinho@gmail.com>
2024-01-08 09:42:59 -0300

    basico

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite a senha!");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002) {
+                Console.WriteLine("Senha invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido.");
+        }
+    }
+}
